d0db11dc069b846cd6ba8195f8f80583c4899f1c
unknown <mehrozali1990@gmail.com>
2020-12-21 19:46:27 +0500

    added scss and style

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+                                                                                                                                                                            import React, { Component } from "react";
 
 class StopWatch extends Component {
   render() {
     return (
       <div className="background-style">
         <h1>00:00:00:00</h1>
+        <p>SPLIT TIME</p>
         <div>  
             <button className="button-style">Start</button>
-            <button>Split</button>
-            <button>Reset</button>
+            <button className="button-style-1">Split</button>
+            <button className="button-style-1">Reset</button>
         </div>
       </div>
     );
