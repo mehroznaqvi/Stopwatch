9fcf1637fb010c7610f1b2e429fd4dae9bf31d28
unknown <mehrozali1990@gmail.com>
2020-12-29 20:10:36 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Style.scss";
-
-//components
-import Timer from "./components/timer";
-
-
-function App() {
-  return(
-    <Timer></Timer>
-
-  );
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
